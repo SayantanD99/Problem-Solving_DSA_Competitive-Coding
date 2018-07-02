feeffcea3410463614d5e306071b03e2772bf77b
Sayantan Das <32683338+SayantanD99@users.noreply.github.com>
2018-07-02 22:52:27 +0530

    Create Separating Odd & Even Indexed Letters of a String.py
    Task : 
    
    Given a string, , of length  that is indexed from  to , print its even-indexed and odd-indexed characters as  space-separated strings on a single line (see the Sample below for more detail).
    
    Note: 0 is considered to be an even index.
    
    Input Format : 
    
    The first line contains an integer,  (the number of test cases). 
    Each line  of the  subsequent lines contain a String, .
    
    Constraints :
    1<=T<=10
    2<Length of S<10000
    
    Output Format : 
    
    For each String  (where 0<J<T-1 ), print 's even-indexed characters, followed by a space, followed by 's odd-indexed characters.

---
+++ Separating Odd & Even Indexed Letters of a String.py
@@ -0,0 +1,36 @@
+"""
+Task : 
+
+Given a string, , of length  that is indexed from  to , print its even-indexed and odd-indexed characters as  space-separated strings on a single line (see the Sample below for more detail).
+
+Note: 0 is considered to be an even index.
+
+Input Format : 
+
+The first line contains an integer,  (the number of test cases). 
+Each line  of the  subsequent lines contain a String, .
+
+Constraints :
+1<=T<=10
+2<Length of S<10000
+
+Output Format : 
+
+For each String  (where 0<J<T-1 ), print 's even-indexed characters, followed by a space, followed by 's odd-indexed characters.
+"""
+
+def strman():
+    t=int(input())
+    while  t>0 :
+        s=input()
+        for i in range(0,len(s)):
+            if i%2==0:
+                print(s[i],end="")
+        print(end=" ")
+        for i in range(0,len(s)):
+            if i%2!=0:
+                print(s[i],end="")
+        t=t-1
+        print("")
+
+strman()
