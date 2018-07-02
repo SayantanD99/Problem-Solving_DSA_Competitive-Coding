4c4ce4559ccd11d18ec18fd80fd30b86e2c94d6b
Sayantan Das <32683338+SayantanD99@users.noreply.github.com>
2018-07-02 22:42:00 +0530

    Create sum.c
    /* Complete the function solveMeFirst to compute the sum of two integers.
    
    Function prototype:
    
    int solveMeFirst(int x, int y);
    
    where,
    
    x is the first integer input.
    y is the second integer input
    
    Return values
    
    sum of the above two integers
    
    Sample Input
    
    x = 2
    y = 3
    
    Sample Output
    
    5
    
    Explanation
    
    The sum of the two integers  and  is computed as: 5 */

---
+++ sum.c
@@ -0,0 +1,48 @@
+/* Complete the function solveMeFirst to compute the sum of two integers.
+
+Function prototype:
+
+int solveMeFirst(int x, int y);
+
+where,
+
+x is the first integer input.
+y is the second integer input
+
+Return values
+
+sum of the above two integers
+
+Sample Input
+
+x = 2
+y = 3
+
+Sample Output
+
+5
+
+Explanation
+
+The sum of the two integers  and  is computed as: 5 */
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int solveMeFirst(int a, int b) 
+{
+    return a+b;
+ 
+}
+
+int main() {
+    int num1,num2;
+    printf("Enter two Integers : ");
+    scanf("%d %d",&num1,&num2);
+    int sum; 
+    sum = solveMeFirst(num1,num2);
+    printf("%d",sum);
+    return 0;
+}
