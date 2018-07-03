a221590bb3bc8bb90286331f9b3ac9895ac6cb51
Sayantan Das <32683338+SayantanD99@users.noreply.github.com>
2018-07-03 09:05:55 +0530

    Create arithop.py

---
+++ arithop.py
@@ -0,0 +1,13 @@
+#https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+def cal(A,B):
+    sum=A+B
+    sub=A-B
+    mul=A*B
+    print(sum)
+    print(sub)
+    print(mul)
+cal(a,b)
