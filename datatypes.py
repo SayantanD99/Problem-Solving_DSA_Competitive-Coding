64a4f446789d35a76a78c94f3b79c08d0ab64a6e
Sayantan Das <32683338+SayantanD99@users.noreply.github.com>
2018-07-08 13:53:17 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,37 @@
+"""
+Task :
+
+Complete the code in the editor below. The variables i,d , and s are already declared and initialized for you. You must:
+
+Declare 3 variables:
+one of type int, one of type double, and one of type String.
+
+Read 3 lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your variables.
+
+Use the + operator to perform the following operations: 
+
+  Print the sum of i  plus your int variable on a new line.
+  Print the sum of d plus your double variable to a scale of one decimal place on a new line.
+  Concatenate s  with the string you read as input and print the result on a new line.
+
+Link : https://www.hackerrank.com/challenges/30-data-types/problem
+"""
+def ch():
+    inte=int(input())
+    dou=float(input())
+    st=input()
+    print(inte+i)
+    print(d+dou)
+    print(s+st)
+ch()
+    
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
